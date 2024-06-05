26db081ed4758c4f29f7c796302a4d2afa256e69
n1tr06 <eitangil1016@gmail.com>
2024-06-06 02:09:24 +0300

    string => list :)

--- test.py
+++ test.py
@@ -1,24 +1,25 @@
 import math
 
-def addOneBit(final, num):
+def addOneBit(arr):
 
-    i = final[len(final) - 1]
-    if(i == "0"):
-        newfinal = ""
-
-        for j in range(len(final) - 1, 0,-1): 
-            newfinal += final[j]
-        newfinal += "1"
-    else:
-        count = 1;
-        j = len(final) - 2
-        while i != "0":
-            j -= 1
-            count += 1
-            i = final[j]
+    for i in range(len(arr)-1, -1 , -1):
+        if i == 0:
+            break;
+        elif arr[i] == 0:
+            arr[i] = 1
+            break;
+        else:
+            arr[i] = 0
+    return arr
             
-    final = newfinal
-    print(final)
+def reverseBit(arr):
+    for i in range(0, len(arr)):
+        if arr[i] == 0:
+            arr[i] = 1
+        else:
+            arr[i] = 0
+    return arr
+
 
 
 num = int(input("Enter a number "))
@@ -28,7 +29,7 @@ while (2 ** (bit - 1)) - 1 < num:
 
     print(f"The number is too big for your bit amount, you need at least {math.ceil(math.log2(num + 1)) + 1} bits ")
     print("1. change number ")
-    x = int(input("2. complete bit amount "))
+    x = int(input("2. complete bit amount \n"))
 
     while x != 1 and x != 2:
         x = input("entered wrong answer, please enter 1 or 2 ")
@@ -37,8 +38,12 @@ while (2 ** (bit - 1)) - 1 < num:
         case 1: num = int(input("Enter a number "))
         case 2: bit = math.ceil(math.log2(num + 1)) + 1
 
-final = ""
-for i in range(bit):
-    final += "0"
+final = [0] * bit
 
-addOneBit(final, num)
+for i in range(0, num):
+    final = addOneBit(final)
+
+final = reverseBit(final)
+print(addOneBit(final))
+
+
