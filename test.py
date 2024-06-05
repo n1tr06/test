83d27fbbc424c231c7ebedfd305661d5fa37a558
n1tr06 <eitangil1016@gmail.com>
2024-06-06 02:51:34 +0300

    0_0

--- test.py
+++ test.py
@@ -45,19 +45,19 @@ bit = int(input("Enter bit amount: "))
 
 while (2 ** (bit - 1)) - 1 < num:
 
-    print(f"The number is too big for your bit amount, you need at least {math.ceil(math.log2(num + 1)) + 1} bits.")
+    print(f"The number is too big for your bit amount, you need at least {math.ceil(math.log2(num + 1)) + 2} bits.")
     print("1. Change number")
     print("2. Change bit amount")
-    print(f"3. Complete bit amount ({math.ceil(math.log2(num + 1)) + 1})")
+    print(f"3. Complete bit amount ({math.ceil(math.log2(num + 1)) + 2})")
     x = int(input("Enter 1, 2 or 3: "))
 
-    while x != 1 and x != 2:
+    while x != 1 and x != 2 and x != 3:
         x = input("Entered wrong answer, please enter 1 or 2: ")
 
     match x:
         case 1: num = int(input("Enter a number: "))
         case 2: bit = int(input("Enter bit amount: "))
-        case 3: bit = math.ceil(math.log2(num + 1)) + 1
+        case 3: bit = math.ceil(math.log2(num + 1)) + 2
 
 final = toBinList(num, bit)
 
